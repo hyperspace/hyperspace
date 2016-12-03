f3a96e45058c695573e04e43814eb7bd7f5c98af
zehfernandes <ozehfernandes@gmail.com>
2016-12-03 20:16:28 -0200

    Open files ¯\_(ツ)_/¯

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 import configs from './src/vtex-audit.json'
+import open from 'open'
+import _ from 'lodash'
 
-console.log(configs)
+const openFiles = (file, name) => {
+
+    let t = open(file, name, (error) => {
+        if (error) {
+            let msg = error.toString().split('\n')
+            console.log(msg[1])
+        }
+    })
+
+}
+
+const windows = configs.windows
+
+_.forEach(windows, (app, name) => {
+
+    _.forEach(app.files, (file) => {
+        setTimeout(() => {
+            openFiles(file,name)
+        }, 100)
+    })
+
+})
