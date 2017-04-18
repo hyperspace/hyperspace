3e8fd1d7353b06a6114201ad20198152253cb775
zehfernandes <ozehfernandes@gmail.com>
2017-04-18 15:10:58 -0300

    return displays and spaces available

--- src/configs/phoenix.js
+++ src/configs/phoenix.js
@@ -7,63 +7,63 @@ Phoenix.set({
 
 //full, top, bottom, left, right, top-right, top-left bottom-right, bottom-left
 function screenPosition(window, sysPosition) {
-	 //Check what screen
-	 let windowRect = Screen.main().flippedFrame()
+   //Check what screen
+   let windowRect = Screen.main().flippedFrame()
 
-	 if( sysPosition.indexOf("top") > -1 ) {
-		windowRect.height = windowRect.height / 2
-	 }
+   if( sysPosition.indexOf("top") > -1 ) {
+    windowRect.height = windowRect.height / 2
+   }
 
-	 if( sysPosition.indexOf("bottom") > -1 ) {
-		windowRect.y = windowRect.height / 2
-		windowRect.height = windowRect.height / 2
-	 }
+   if( sysPosition.indexOf("bottom") > -1 ) {
+    windowRect.y = windowRect.height / 2
+    windowRect.height = windowRect.height / 2
+   }
 
-	 if( sysPosition.indexOf("left") > -1 ) {
-		windowRect.width = windowRect.width / 2
-	 }
+   if( sysPosition.indexOf("left") > -1 ) {
+    windowRect.width = windowRect.width / 2
+   }
 
-	 if( sysPosition.indexOf("right") > -1 ) {
-		windowRect.x = windowRect.width / 2
-		windowRect.width = windowRect.width / 2
-	 }
+   if( sysPosition.indexOf("right") > -1 ) {
+    windowRect.x = windowRect.width / 2
+    windowRect.width = windowRect.width / 2
+   }
 
-	window.setFrame(windowRect)
+  window.setFrame(windowRect)
 }
 
 /* Checklog */
 function isDone() {
-	var sysWindows = Storage.get('sysWindows')
-	var allinPosition = []
+  var sysWindows = Storage.get('sysWindows')
+  var allinPosition = []
 
-	sysWindows.forEach(function(windowApp, index) {
-		const key = `${windowApp.app}-${index}`
-		const inPosition = Storage.get(key)
+  sysWindows.forEach(function(windowApp, index) {
+    const key = `${windowApp.app}-${index}`
+    const inPosition = Storage.get(key)
 
-		allinPosition.push(inPosition)
+    allinPosition.push(inPosition)
 
-	}, this);
+  }, this);
 
-	var isDone = allinPosition.every(elem => elem === true);
+  var isDone = allinPosition.every(elem => elem === true);
 
-	if(isDone) Phoenix.log("DONE")
+  if(isDone) Phoenix.log("DONE")
 };
 
 /* Spaces */
 function moveWindowToTargetSpace(target, windowConfig, key) {
-	const allSpaces 	= Space.all(),
-		  pos 			= windowConfig.position,
-		  spaceIndex 	= windowConfig.space
+  const allSpaces 	= Space.all(),
+      pos 			= windowConfig.position,
+      spaceIndex 	= windowConfig.space
 
-	var targetSpace = allSpaces[spaceIndex]
-	var currentSpace = target.spaces()[0]
+  var targetSpace = allSpaces[spaceIndex]
+  var currentSpace = target.spaces()[0]
 
-	currentSpace.removeWindows([target])
-	targetSpace.addWindows([target])
+  currentSpace.removeWindows([target])
+  targetSpace.addWindows([target])
 
-	screenPosition(target, pos)
+  screenPosition(target, pos)
 
-	Storage.set(key, true)
+  Storage.set(key, true)
 };
 
 /* --------
@@ -71,49 +71,64 @@ Binds
 -------- */
 
 var bindStorage = new Key('s', [ 'ctrl', 'shift' ], function () {
-	require('appTemp.js')
-	Phoenix.reload()
+  require('appTemp.js')
+  Phoenix.reload()
 
-	Phoenix.log("storage")
+  Phoenix.log("storage")
 
-	Storage.set('sysWindows', sysWindows)
+  Storage.set('sysWindows', sysWindows)
 
-	sysWindows.forEach(function(windowApp, index) {
-		//var obj = Object.assign(windowApp, { open: false })
-		const key = `${windowApp.app}-${index}`
-		Storage.set(key, false)
-	}, this);
+  sysWindows.forEach(function(windowApp, index) {
+    //var obj = Object.assign(windowApp, { open: false })
+    const key = `${windowApp.app}-${index}`
+    Storage.set(key, false)
+  }, this);
 
+});
+
+var bindDisplays = new Key('d', [ 'ctrl', 'shift' ], function () {
+    var display = Screen.all()
+    Phoenix.log("DISPLAY:"+display.length)
+});
+
+var bindSpaces = new Key('g', [ 'ctrl', 'shift' ], function () {
+    var displays = Screen.all()
+    var returnObj = []
+    displays.forEach(function(dply, index) {
+        var spaces = dply.spaces()
+        returnObj.push(spaces.length)
+    }, this)
+
+    Phoenix.log("SPACE:"+JSON.stringify(returnObj))
 });
 
 /* Position */
 var bindPosition = new Key('p', [ 'ctrl', 'shift' ], function () {
-	var sysWindows = Storage.get('sysWindows')
+  var sysWindows = Storage.get('sysWindows')
 
-	sysWindows.forEach(function(windowApp, index) {
-		const key = `${windowApp.app}-${index}`
-		const inPosition = Storage.get(key)
+  sysWindows.forEach(function(windowApp, index) {
+    const key = `${windowApp.app}-${index}`
+    const inPosition = Storage.get(key)
 
-		if (inPosition) {
-			return
-		}
+    if (inPosition) {
+      return
+    }
 
-		Phoenix.log("Status " + JSON.stringify(inPosition))
-		const app = App.get(windowApp.app)
-		Phoenix.log(app.name())
+    Phoenix.log("Status " + JSON.stringify(inPosition))
+    const app = App.get(windowApp.app)
+    Phoenix.log(app.name())
 
-		if( app === undefined) return
+    if (app === undefined) return
 
-		//To-do: Find the right window
-		let targetWindow = app.mainWindow()
+    // To-do: Find the right window
+    let targetWindow = app.mainWindow()
 
-		if( targetWindow === undefined ) return
+    if (targetWindow === undefined) return
 
-		//To-do: Promisse
-		moveWindowToTargetSpace(targetWindow, windowApp, key)
+    // To-do: Promisse
+    moveWindowToTargetSpace(targetWindow, windowApp, key)
+  }, this)
 
-	}, this);
 
-
-	isDone()
-});
+  isDone()
+})
