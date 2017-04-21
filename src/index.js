d701496353037628190fc0807a172bc7a069ae74
Breno Calazans <brenocalazans@gmail.com>
2017-04-21 15:58:29 -0300

    Fix prettier hook to account for eslint rules

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const _ = require("lodash");
-const fs = require("fs");
-const osascript = require("node-osascript");
-const setup = require("./setup");
-const exec = require("child_process");
-const getHomePath = require("home-path");
+const _ = require('lodash')
+const fs = require('fs')
+const osascript = require('node-osascript')
+const setup = require('./setup')
+const exec = require('child_process')
+const getHomePath = require('home-path')
 
-const HOME = getHomePath();
-let loopTimer;
+const HOME = getHomePath()
+let loopTimer
 
 /*
     Get Informations
@@ -14,89 +14,89 @@ let loopTimer;
 
 const getPhoenixData = args => {
   return new Promise(resolve => {
-    let appleScript = `tell application "System Events" to keystroke "${args.keystroke}" using {control down, shift down}`;
-    osascript.execute(appleScript);
+    let appleScript = `tell application "System Events" to keystroke "${args.keystroke}" using {control down, shift down}`
+    osascript.execute(appleScript)
 
     // eslint-disable-next-line
-    let child = exec.spawn("log", [
-      "stream",
-      "--style",
-      "json",
-      "--predicate",
+    let child = exec.spawn('log', [
+      'stream',
+      '--style',
+      'json',
+      '--predicate',
       `eventMessage contains "hyperspace-${args.message}"`,
-      "--info"
-    ]);
+      '--info',
+    ])
 
-    child.stdout.on("data", function(data) {
-      const message = JSON.parse(data.toString()).eventMessage;
-      const result = args.parse(message.split(":")[1]);
-      child.kill("SIGKILL");
-      resolve(result);
-    });
-  });
-};
+    child.stdout.on('data', function(data) {
+      const message = JSON.parse(data.toString()).eventMessage
+      const result = args.parse(message.split(':')[1])
+      child.kill('SIGKILL')
+      resolve(result)
+    })
+  })
+}
 
 const writePhoenixObject = object => {
-  const template = fs.readFileSync("./src/template/appTemp.js", "utf8");
-  const render = _.template(template);
-  const file = render(object);
+  const template = fs.readFileSync('./src/template/appTemp.js', 'utf8')
+  const render = _.template(template)
+  const file = render(object)
 
   // FIX: Hard-code
-  fs.writeFileSync(HOME + "/.config/phoenix/appTemp.js", file);
+  fs.writeFileSync(HOME + '/.config/phoenix/appTemp.js', file)
 
   // Time to open windows files (fail safe)
   setTimeout(function() {
-    setStorage();
-  }, 500);
-};
+    setStorage()
+  }, 500)
+}
 
 const setStorage = () => {
   let appleScript =
-    'tell application "System Events" to keystroke "s" using {control down, shift down}';
-  osascript.execute(appleScript);
+    'tell application "System Events" to keystroke "s" using {control down, shift down}'
+  osascript.execute(appleScript)
 
-  loopTimer = setInterval(positionWaitLoop, 2000);
+  loopTimer = setInterval(positionWaitLoop, 2000)
 
   // eslint-disable-next-line
-  let child = exec.spawn("log", [
-    "stream",
-    "--style",
-    "json",
-    "--predicate",
+  let child = exec.spawn('log', [
+    'stream',
+    '--style',
+    'json',
+    '--predicate',
     'eventMessage contains "hyperspace-DONE"',
-    "--info"
-  ]);
+    '--info',
+  ])
 
-  child.stdout.on("data", function() {
-    clearInterval(loopTimer);
-    child.kill("SIGKILL");
-  });
-};
+  child.stdout.on('data', function() {
+    clearInterval(loopTimer)
+    child.kill('SIGKILL')
+  })
+}
 
 const positionWaitLoop = () => {
   let appleScript =
-    'tell application "System Events" to keystroke "p" using {control down, shift down}';
-  osascript.execute(appleScript);
-};
+    'tell application "System Events" to keystroke "p" using {control down, shift down}'
+  osascript.execute(appleScript)
+}
 
 getPhoenixData({
-  message: "DISPLAY",
-  keystroke: "d",
-  parse: value => parseInt(value)
+  message: 'DISPLAY',
+  keystroke: 'd',
+  parse: value => parseInt(value),
 }).then(data => {
-  console.log("Yay! " + data);
-});
+  console.log('Yay! ' + data)
+})
 
 getPhoenixData({
-  message: "SPACE",
-  keystroke: "g",
-  parse: value => value.replace(/[[\]']+/g, "").split(",")
+  message: 'SPACE',
+  keystroke: 'g',
+  parse: value => value.replace(/[[\]']+/g, '').split(','),
 }).then(data => {
-  console.log("Yay! " + data);
-});
+  console.log('Yay! ' + data)
+})
 
 /* Controler */
-setup();
+setup()
 
-const windowsConfigs = [];
-writePhoenixObject({ windows: windowsConfigs });
+const windowsConfigs = []
+writePhoenixObject({windows: windowsConfigs})
--- src/setup.js
+++ src/setup.js
@@ -1,40 +1,40 @@
-const open = require("open");
-const getHomePath = require("home-path");
-const HOME = getHomePath();
+const open = require('open')
+const getHomePath = require('home-path')
+const HOME = getHomePath()
 
 module.exports = function setup(project) {
   if (!project) {
-    project = require("./mocks/project-template.json");
+    project = require('./mocks/project-template.json')
   }
 
-  const toOpen = getProjectWindowsFiles(project);
+  const toOpen = getProjectWindowsFiles(project)
 
-  toOpen.map(openFile);
-};
+  toOpen.map(openFile)
+}
 
 function openFile(window) {
   open(window.file, window.name, error => {
     if (error) {
-      let msg = error.toString().split("\n");
-      console.log(msg[1]);
+      let msg = error.toString().split('\n')
+      console.log(msg[1])
 
-      return;
+      return
     }
-  });
+  })
 }
 
 function getProjectWindowsFiles(project) {
   return project.windows.reduce((filesToOpen, window) => {
-    const windowFiles = getWindowFiles(window);
-    return filesToOpen.concat(windowFiles);
-  }, []);
+    const windowFiles = getWindowFiles(window)
+    return filesToOpen.concat(windowFiles)
+  }, [])
 }
 
 function getWindowFiles(window) {
   return window.files.reduce((itemFiles, filePath) => {
-    const fileObj = buildFileObj(window, filePath);
-    return itemFiles.concat(fileObj);
-  }, []);
+    const fileObj = buildFileObj(window, filePath)
+    return itemFiles.concat(fileObj)
+  }, [])
 }
 
 function buildFileObj(window, filePath) {
@@ -43,10 +43,10 @@ function buildFileObj(window, filePath) {
     name: window.app,
     pos: window.position,
     space: window.space,
-    display: window.display
-  };
+    display: window.display,
+  }
 }
 
 function replaceHomePath(filePath) {
-  return filePath.replace(/^~/, HOME);
+  return filePath.replace(/^~/, HOME)
 }
