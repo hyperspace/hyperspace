6346dbf3d01fd650e9576968e5f1385bc9d9cfa9
Breno Calazans <brenocalazans@gmail.com>
2017-04-21 19:23:08 -0300

    Implement display adaptor

--- src/open/displays.js
+++ src/open/displays.js
@@ -4,9 +4,7 @@ module.exports = function adaptProjectToAvailableDisplays(project) {
   console.log('Adapting to available displays')
 
   return getNumberOfDisplays().then(numberOfDisplays => {
-    console.log('Number of displays', numberOfDisplays)
-
-    return project
+    return adaptWindows(project, numberOfDisplays)
   })
 }
 
@@ -16,4 +14,19 @@ function getNumberOfDisplays() {
     keystroke: 'd',
     parse: value => parseInt(value),
   })
+}
+
+function adaptWindows(project, numberOfDisplays) {
+  const adaptedWindows = project.windows.map(window => {
+    if (window.display > numberOfDisplays) {
+      return Object.assign({}, window, {
+        display: numberOfDisplays,
+      })
+    }
+    return window
+  })
+
+  project.windows = adaptedWindows
+
+  return project
 }
