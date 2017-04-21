ad3ad71215ac539f29909f3d8f7b4fa23e61aa7c
Breno Calazans <brenocalazans@gmail.com>
2017-04-21 17:54:45 -0300

    Refactor Phoenix script

--- src/configs/phoenix.js
+++ src/configs/phoenix.js
@@ -1,165 +1,171 @@
-/* global Phoenix, App, Space, Key */
-"use strict";
+/* global Phoenix, App, Space, Key, sysWindows */
+'use strict'
 
 Phoenix.set({
-  daemon: true,
-  openAtLogin: true
-});
+  daemon: false,
+  openAtLogin: true,
+})
 
 // full, top, bottom, left, right, top-right, top-left bottom-right, bottom-left
 function screenPosition(window, sysPosition) {
   // Check what screen
-  const windowRect = Screen.main().flippedFrame();
+  const windowRect = Screen.main().flippedFrame()
 
-  if (sysPosition.indexOf("top") > -1) {
-    windowRect.height = windowRect.height / 2;
+  if (sysPosition.indexOf('top') > -1) {
+    windowRect.height = windowRect.height / 2
   }
 
-  if (sysPosition.indexOf("bottom") > -1) {
-    windowRect.y = windowRect.height / 2;
-    windowRect.height = windowRect.height / 2;
+  if (sysPosition.indexOf('bottom') > -1) {
+    windowRect.y = windowRect.height / 2
+    windowRect.height = windowRect.height / 2
   }
 
-  if (sysPosition.indexOf("left") > -1) {
-    windowRect.width = windowRect.width / 2;
+  if (sysPosition.indexOf('left') > -1) {
+    windowRect.width = windowRect.width / 2
   }
 
-  if (sysPosition.indexOf("right") > -1) {
-    windowRect.x = windowRect.width / 2;
-    windowRect.width = windowRect.width / 2;
+  if (sysPosition.indexOf('right') > -1) {
+    windowRect.x = windowRect.width / 2
+    windowRect.width = windowRect.width / 2
   }
 
-  window.setFrame(windowRect);
+  window.setFrame(windowRect)
 }
 
 /* Checklog */
 function isDone() {
-  const sysWindows = Storage.get("sysWindows");
-  const allinPosition = [];
+  const sysWindows = Storage.get('sysWindows')
 
-  sysWindows.forEach(function(windowApp, index) {
-    const key = `${windowApp.app}-${index}`;
-    const inPosition = Storage.get(key);
+  const notDoneYet = sysWindows.filter(function(windowApp, index) {
+    const key = `${windowApp.app}-${index}`
+    const inPosition = Storage.get(key)
 
-    allinPosition.push(inPosition);
-  });
+    return inPosition === false
+  })
 
-  var isDone = allinPosition.every(elem => elem === true);
+  var isDone = notDoneYet.length === 0
 
-  if (isDone) Phoenix.log("hyperspace-DONE");
+  if (isDone) broadcast('DONE', '')
 }
 
+/* Binds */
+function onStoreWindows() {
+  require('appTemp.js') // sysWindows come from here
+  Phoenix.reload()
+
+  Phoenix.log('storage')
+
+  Storage.set('sysWindows', sysWindows)
+
+  sysWindows.forEach(function(windowApp, index) {
+    const key = `${windowApp.app}-${index}`
+    Storage.set(key, false)
+  })
+}
+
+Key.on('s', ['ctrl', 'shift'], onStoreWindows)
+
+function broadcast(name, data) {
+  Phoenix.log(`hyperspace-${name}||${JSON.stringify(data)}`)
+}
+
+function getNumberOfDisplays() {
+  return Screen.all().length
+}
+
+function onGetNumberOfDisplays() {
+  broadcast('DISPLAY', getNumberOfDisplays())
+}
+
+Key.on('d', ['ctrl', 'shift'], onGetNumberOfDisplays)
+
+function onGetNumberOfSpacesPerDisplay() {
+  const displays = Screen.all()
+
+  const returnObj = displays.map(function(display) {
+    let spaces = display.spaces()
+    return spaces.length
+  })
+
+  broadcast('SPACE', returnObj)
+}
+
+Key.on('g', ['ctrl', 'shift'], onGetNumberOfSpacesPerDisplay)
+
+/* Position */
+function onPositionWindows() {
+  var sysWindows = Storage.get('sysWindows')
+
+  sysWindows.forEach(function(windowApp, index) {
+    const key = `${windowApp.app}-${index}`
+    const inPosition = Storage.get(key)
+
+    if (inPosition) {
+      return
+    }
+
+    Phoenix.log('Status ' + JSON.stringify(inPosition))
+    const app = App.get(windowApp.app)
+
+    if (app === undefined) return
+    Phoenix.log(app.name())
+
+    // To-do: Find the right window
+    let targetWindow = app.mainWindow()
+    if (targetWindow === undefined) return
+
+    // To-do: Promise
+    moveWindowToTargetSpace(targetWindow, windowApp, key)
+  })
+
+  isDone()
+}
+
+function moveWindowToTargetSpace(target, windowConfig, key) {
+  const allSpaces = Space.all()
+  const pos = windowConfig.position
+  const spaceIndex = windowConfig.space
+
+  var targetSpace = allSpaces[spaceIndex]
+  var currentSpace = target.spaces()[0]
+
+  currentSpace.removeWindows([target])
+  targetSpace.addWindows([target])
+
+  screenPosition(target, pos)
+
+  Storage.set(key, true)
+}
+
+Key.on('p', ['ctrl', 'shift'], onPositionWindows)
+
 function getCurrentHyperSpace() {
-  let displays = Screen.all();
+  let displays = Screen.all()
   let spaces = displays.map(function(d) {
-    return d.spaces();
-  });
+    return d.spaces()
+  })
 
   const config = {
     displays: displays.length,
-    spaces: spaces
-  };
+    spaces: spaces,
+  }
 
-  const windowsObj = [];
+  const windowsObj = []
   displays.map(function(d) {
-    d.windows().forEach(function(windowApp, index) {
+    d.windows().forEach(function(windowApp) {
       windowsObj.push({
         app: windowApp.app().name(),
         position: windowApp.frame(),
         space: windowApp.spaces(),
-        display: windowApp.screen()
-      });
-    });
-  });
-
-  Phoenix.log(
-    "hyperspace-ENV:" +
-      JSON.stringify({
-        config: config,
-        windows: windowsObj
+        display: windowApp.screen(),
       })
-  );
+    })
+  })
+
+  broadcast('ENV', {
+    config: config,
+    windows: windowsObj,
+  })
 }
 
-/* Spaces */
-function moveWindowToTargetSpace(target, windowConfig, key) {
-  const allSpaces = Space.all();
-  const pos = windowConfig.position;
-  const spaceIndex = windowConfig.space;
-
-  var targetSpace = allSpaces[spaceIndex];
-  var currentSpace = target.spaces()[0];
-
-  currentSpace.removeWindows([target]);
-  targetSpace.addWindows([target]);
-
-  screenPosition(target, pos);
-
-  Storage.set(key, true);
-}
-
-/* Binds */
-Key.on("s", ["ctrl", "shift"], function() {
-  require("appTemp.js");
-  Phoenix.reload();
-
-  Phoenix.log("storage");
-
-  Storage.set("sysWindows", sysWindows);
-
-  sysWindows.forEach(function(windowApp, index) {
-    // var obj = Object.assign(windowApp, { open: false })
-    const key = `${windowApp.app}-${index}`;
-    Storage.set(key, false);
-  }, this);
-});
-
-Key.on("d", ["ctrl", "shift"], function() {
-  var display = Screen.all();
-  Phoenix.log("hyperspace-DISPLAY:" + display.length);
-});
-
-Key.on("g", ["ctrl", "shift"], function() {
-  var displays = Screen.all();
-  var returnObj = [];
-  displays.forEach(function(dply) {
-    var spaces = dply.spaces();
-    returnObj.push(spaces.length);
-  }, this);
-
-  Phoenix.log("hyperspace-SPACE:" + JSON.stringify(returnObj));
-});
-
-/* Position */
-Key.on("p", ["ctrl", "shift"], function() {
-  var sysWindows = Storage.get("sysWindows");
-
-  sysWindows.forEach(function(windowApp, index) {
-    const key = `${windowApp.app}-${index}`;
-    const inPosition = Storage.get(key);
-
-    if (inPosition) {
-      return;
-    }
-
-    Phoenix.log("Status " + JSON.stringify(inPosition));
-    const app = App.get(windowApp.app);
-    Phoenix.log(app.name());
-
-    if (app === undefined) return;
-
-    // To-do: Find the right window
-    let targetWindow = app.mainWindow();
-
-    if (targetWindow === undefined) return;
-
-    // To-do: Promisse
-    moveWindowToTargetSpace(targetWindow, windowApp, key);
-  }, this);
-
-  isDone();
-});
-
-Key.on("a", ["ctrl", "shift"], function() {
-  getCurrentHyperSpace();
-});
+Key.on('a', ['ctrl', 'shift'], getCurrentHyperSpace)
