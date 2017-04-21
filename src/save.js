fed426ccfb2e379297ac9a70ea7710a2bd69abb9
zehfernandes <ozehfernandes@gmail.com>
2017-04-21 20:28:10 -0300

    Save current env

--- src/save.js
+++ src/save.js
@@ -1,22 +1,34 @@
-const getPhoenixData = require('./phoenixTunnel.js')
+const {getPhoenixData} = require('./phoenixTunnel.js')
 const getHomePath = require('home-path')
+const path = require('path')
+const fs = require('fs')
 const HOME = getHomePath()
 
-module.exports = function save() {
-  await getPhoenixData({
-    message:
-    keystroke:
-    parse:
-  })
+/* module.exports =*/
+async function save() {
+  const projectFilePath = path.join(
+    HOME,
+    '.config',
+    'hyperspace',
+    'project-template.js',
+  )
 
+  try {
+    const data = await getPhoenixData({
+      message: 'ENV',
+      keystroke: 'a',
+      parse: value => value,
+    })
+
+    fs.writeFileSync(projectFilePath, data)
+    // console.log(JSON.parse(data))
+    // console.log(JSON.parse(data))
+  } catch (error) {
+    console.error(error)
+  }
 }
 
-function get() {
-
-}
-
-
+save()
 // get the json
 // save in hyperspace folder
 // use template hehe
-
