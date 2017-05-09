fc73a82630a08f51f5c39290f902b7526a3a9547
zehfernandes <ozehfernandes@gmail.com>
2017-05-09 15:20:35 -0300

    Add missing close file

---
+++ src/open/closeApps.js
@@ -0,0 +1,35 @@
+const osascript = require('node-osascript')
+
+module.exports = function closeApps(project) {
+  console.log('Closing apps')
+  const whiteList = `"${project.keepApps.join('","')}"`
+  return new Promise(resolve => {
+    const appleScript = `tell application "System Events" to set the visible of every process to true
+
+    set white_list to {${whiteList}}
+
+    tell application "Finder"
+      set process_list to the name of every process whose visible is true
+    end tell
+    repeat with i from 1 to (number of items in process_list)
+      set this_process to item i of the process_list
+      if this_process is not in white_list then
+        tell application this_process
+          quit
+        end tell
+      end if
+    end repeat
+    delay 0.5
+    tell application "Finder" to activate
+    `
+
+    osascript.execute(appleScript, function(err, result, raw) {
+      if (err) {
+        console.error(err)
+        return
+      }
+
+      resolve(project)
+    })
+  })
+}
