51573100ba70a9410fa110096b5d8513c8ab1fd5
zehfernandes <ozehfernandes@gmail.com>
2017-05-09 13:36:28 -0300

    Find the right opener #2

--- src/open/openers/default.js
+++ src/open/openers/default.js
@@ -1,6 +1,18 @@
 const open = require('open')
+const fs = require('fs')
 
-module.exports = function openFile(window) {
+module.exports = function pipeOpener(window) {
+  let app = window.name.replace(/\s/g, '').toLowerCase()
+  let openerPath = `${__dirname}/open-${app}.js`
+  if (fs.existsSync(openerPath)) {
+    const opener = require(openerPath)
+    return opener(window)
+  }
+
+  return defaultOpen(window)
+}
+
+function defaultOpen(window) {
   open(window.file, window.name, error => {
     if (error) {
       let msg = error.toString().split('\n')
