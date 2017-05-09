166fbb7b2edd74e70f1e6ed614738247ec664b63
zehfernandes <ozehfernandes@gmail.com>
2017-05-09 16:20:39 -0300

    Create install process

--- src/configs/setup.sh
+++ src/configs/setup.sh
@@ -1,10 +1,28 @@
-if [ -f "/Applications/Phoenix.app" ]
+#!/usr/bin/env bash
+
+FILE=/Applications/Phoenix.app
+
+if [ ! -d $FILE ]
 then
+    echo "Install Phoenix.app"
+    path=${PWD}
+    cd /Applications && curl -L 'https://github.com/kasper/phoenix/releases/download/2.5/phoenix-2.5.tar.gz' | tar -xz
+    wait
+    echo "Creating config files"
+    if [ ! -d ~/.config/phoenix/ ]
+    then
+      mkdir ~/.config/phoenix/
+    fi
+    if [ ! -d ~/.config/hyperspace/ ]
+    then
+      mkdir ~/.config/hyperspace/
+    fi
+    wait
+    cp $path/src/configs/phoenix.js ~/.config/phoenix/phoenix.js
+    wait
     open /Applications/Phoenix.app
 else
-    echo "start."
-    #cd /Applications && curl -L 'https://github.com/kasper/phoenix/releases/download/2.3/phoenix-2.3.tar.gz' | tar -xz
-    #wait
-    #open /Applications/Phoenix.app
+    echo "Open Phoenix.app"
+    open /Applications/Phoenix.app
 fi
 
