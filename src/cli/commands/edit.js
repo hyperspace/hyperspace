02a00be904ba6d0d9749b8c2a273c032b64df3e2
zehfernandes <ozehfernandes@gmail.com>
2017-05-13 20:16:27 -0300

    Create edit command

--- src/cli/commands/edit.js
+++ src/cli/commands/edit.js
@@ -1,1 +1,34 @@
-module.exports = {}
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const open = require('open')
+const { projectsDirPath, getAllProjects } = require('../../lib/projects')
+
+module.exports = {
+  description: 'Edit a project',
+  optionalArgs: 'projectName',
+  handler(projectName) {
+    console.log(projectName)
+    if (projectName) {
+      return editFile(projectName)
+    }
+
+    return askWhatProject()
+  },
+}
+
+function askWhatProject() {
+  return getListofProjects().then(editFile)
+}
+
+function getListofProjects() {
+  return inquirer.prompt({
+    type: 'list',
+    name: 'project',
+    message: 'Edit project',
+    choices: getAllProjects().map(project => project.replace('.js', '')),
+  })
+}
+
+function editFile(res) {
+  open(`${projectsDirPath}/${res.project}.js`)
+}
