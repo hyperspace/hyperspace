1e908170da4c459295318bcb18b52122cda04350
zehfernandes <ozehfernandes@gmail.com>
2017-05-13 19:20:52 -0300

    Setup always check the configs folder

--- src/configs/setup.sh
+++ src/configs/setup.sh
@@ -8,21 +8,25 @@ then
     path=${PWD}
     cd /Applications && curl -L 'https://github.com/kasper/phoenix/releases/download/2.5/phoenix-2.5.tar.gz' | tar -xz
     wait
-    echo "Creating config files"
-    if [ ! -d ~/.config/phoenix/ ]
-    then
-      mkdir ~/.config/phoenix/
-    fi
-    if [ ! -d ~/.config/hyperspace/ ]
-    then
-      mkdir ~/.config/hyperspace/
-    fi
-    wait
-    cp $path/src/configs/phoenix.js ~/.config/phoenix/phoenix.js
-    wait
-    open /Applications/Phoenix.app
-else
-    echo "Open Phoenix.app"
-    open /Applications/Phoenix.app
 fi
+
+if [ ! -d ~/.config/phoenix/ ]
+then
+  echo "Creating config files"
+  mkdir ~/.config/phoenix/
+fi
+
+if [ ! -d ~/.config/hyperspace/ ]
+then
+  mkdir ~/.config/hyperspace/
+fi
+
+wait
+
+cp $path/src/configs/phoenix.js ~/.config/phoenix/phoenix.js
+
+wait
+echo "Open Phoenix.app"
+open /Applications/Phoenix.app
+
 
