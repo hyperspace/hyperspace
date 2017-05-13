d9a386f7677192789e41f20e07665ec52b7e7614
zehfernandes <ozehfernandes@gmail.com>
2017-05-13 20:57:13 -0300

    create open command

--- src/cli/commands/open.js
+++ src/cli/commands/open.js
@@ -1,1 +1,44 @@
-module.exports = {}
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const open = require('../../open/index')
+const { projectsDirPath, getAllProjects } = require('../../lib/projects')
+
+module.exports = {
+  description: 'Open a project',
+  optionalArgs: 'projectName',
+  handler(projectName) {
+    if (projectName) {
+      if (projectName.split('.').pop() === 'json') { return openProject(projectName) }
+
+      return openProject({ project: projectName })
+    }
+
+    return askWhatProject()
+  },
+}
+
+function askWhatProject() {
+  return getListofProjects().then(openProject)
+}
+
+function getListofProjects() {
+  return inquirer.prompt({
+    type: 'list',
+    name: 'project',
+    message: chalk.bold('What project do you want to travel?'),
+    choices: getAllProjects().map(project => project.replace('.json', '')),
+  })
+}
+
+function openProject(res) {
+  return new Promise(function(resolve, reject) {
+    let filePath = `${projectsDirPath}/${res.project}.json`
+    if (typeof res != 'object') {
+      filePath = res
+    }
+
+    project = require(filePath)
+
+    open(project)
+  })
+}
