8d0ee645a6268e455e64d1c6d3dc561fd9abf54d
zehfernandes <ozehfernandes@gmail.com>
2017-05-13 22:31:22 -0300

    create delete command

--- src/cli/commands/delete.js
+++ src/cli/commands/delete.js
@@ -1,1 +1,37 @@
-module.exports = {}
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const fs = require('fs')
+const { projectsDirPath, getAllProjects } = require('../../lib/projects')
+
+module.exports = {
+  description: 'Delete a project',
+  optionalArgs: 'projectName',
+  handler(projectName) {
+    if (projectName) {
+      return deleteFile({ project: projectName })
+    }
+
+    return askWhatProject()
+  },
+}
+
+function askWhatProject() {
+  return getListofProjects().then(deleteFile)
+}
+
+function getListofProjects() {
+  return inquirer.prompt({
+    type: 'list',
+    name: 'project',
+    message: chalk.bold('Delete project'),
+    choices: getAllProjects().map(project => project.replace('.json', '')),
+  })
+}
+
+function deleteFile(res) {
+  return new Promise(function(resolve, reject) {
+    fs.unlinkSync(`${projectsDirPath}/${res.project}.json`)
+    console.log('')
+    console.log(`Project ${res.project} was removed`)
+  })
+}
